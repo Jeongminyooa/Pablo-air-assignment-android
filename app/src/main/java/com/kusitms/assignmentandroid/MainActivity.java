39f6c017693eb7be69dbbed2f4d39787a283f665
Jeongminyooa <jmjmjmz732002@naver.com>
2022-09-17 02:13:12 +0900

    FEAT : QR Scan

--- app/src/main/java/com/kusitms/assignmentandroid/MainActivity.java
+++ app/src/main/java/com/kusitms/assignmentandroid/MainActivity.java
@@ -2,13 +2,28 @@ package com.kusitms.assignmentandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnScanner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnScanner = findViewById(R.id.btn_scanner);
+        btnScanner.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ScannerActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
