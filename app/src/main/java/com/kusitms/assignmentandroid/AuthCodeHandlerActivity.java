c6f464dcea7046c9d5b4e02e0ff3fc3ae311bd81
Jeongminyooa <jmjmjmz732002@naver.com>
2022-09-17 18:05:39 +0900

    FEAT : 카카오 로그인 구현중

---
+++ app/src/main/java/com/kusitms/assignmentandroid/AuthCodeHandlerActivity.java
@@ -0,0 +1,16 @@
+package com.kusitms.assignmentandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AuthCodeHandlerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth_code_handler);
+
+
+    }
+}
--- app/src/main/java/com/kusitms/assignmentandroid/MainActivity.java
+++ app/src/main/java/com/kusitms/assignmentandroid/MainActivity.java
@@ -1,14 +1,29 @@
 package com.kusitms.assignmentandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+
+import com.kakao.sdk.user.UserApiClient;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    final static String TAG = "login";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 로그인 공통 callback 구성
+        UserApiClient.getInstance().loginWithKakaoTalk(MainActivity.this,(oAuthToken, error) -> {
+            if (error != null) {
+                Log.e(TAG, "로그인 실패", error);
+            } else if (oAuthToken != null) {
+                Log.i(TAG, "로그인 성공(토큰) : " + oAuthToken.getAccessToken());
+            }
+            return null;
+        });
     }
+
 }
