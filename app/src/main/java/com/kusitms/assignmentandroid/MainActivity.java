7e4ec25bc6e553e0740ee5425d8b95c9486b1513
Jeongminyooa <jmjmjmz732002@naver.com>
2022-09-19 02:11:37 +0900

    FEAT : Login 화면 구성

--- app/src/main/java/com/kusitms/assignmentandroid/MainActivity.java
+++ app/src/main/java/com/kusitms/assignmentandroid/MainActivity.java
@@ -2,101 +2,28 @@ package com.kusitms.assignmentandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-
-import com.kakao.sdk.auth.model.OAuthToken;
-import com.kakao.sdk.user.UserApi;
-import com.kakao.sdk.user.UserApiClient;
-import com.kakao.sdk.user.model.Account;
-import com.kakao.sdk.user.model.User;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-import kotlin.Unit;
-import kotlin.jvm.functions.Function2;
-
 public class MainActivity extends AppCompatActivity {
-
-    private static final String TAG = "사용자";
-    private Button btnLogin, btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //getHashKey();
-        btnLogin = findViewById(R.id.btnLogin);
-        btnLogout = findViewById(R.id.btnLogout);
+        getHashKey();
 
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                if(UserApiClient.getInstance().isKakaoTalkLoginAvailable(MainActivity.this)){
-                    login();
-                }
-                else{
-                    accountLogin();
-                }
-            }
-        });
-    }
-
-    public void login(){
-        String TAG = "login()";
-        Log.d(TAG, "로그인 : ");
-
-        UserApiClient.getInstance().loginWithKakaoTalk(MainActivity.this,(oAuthToken, error) -> {
-            if (error != null) {
-                Log.e(TAG, "로그인 실패", error);
-            } else if (oAuthToken != null) {
-                Log.i(TAG, "로그인 성공(토큰) : " + oAuthToken.getAccessToken());
-                getUserInfo();
-            }
-            return null;
-        });
-    }
-    public void accountLogin(){
-        String TAG = "accountLogin()";
-        Log.d(TAG, "로그인 : ");
-        UserApiClient.getInstance().loginWithKakaoAccount(MainActivity.this,(oAuthToken, error) -> {
-            Log.d(TAG, "로그인 : 2222");
-            if (error != null) {
-                Log.e(TAG, "로그인 실패", error);
-            } else if (oAuthToken != null) {
-                Log.i(TAG, "로그인 성공(토큰) : " + oAuthToken.getAccessToken());
-                getUserInfo();
-            }
-            return null;
-        });
-    }
-
-    public void getUserInfo(){
-        String TAG = "getUserInfo()";
-        UserApiClient.getInstance().me((user, meError) -> {
-            if (meError != null) {
-                Log.e(TAG, "사용자 정보 요청 실패", meError);
-            } else {
-                System.out.println("로그인 완료");
-                Log.i(TAG, user.toString());
-                {
-                    Log.i(TAG, "사용자 정보 요청 성공" +
-                            "\n회원번호: " + user.getId());
-                }
-                Account user1 = user.getKakaoAccount();
-                System.out.println("사용자 계정" + user1);
-            }
-            return null;
-        });
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
     }
 
     private void getHashKey(){
