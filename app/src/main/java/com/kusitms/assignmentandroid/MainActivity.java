50bba721051ed354be08a1609b1c48f289318086
Jeongminyooa <jmjmjmz732002@naver.com>
2022-09-19 17:29:11 +0900

    REMOVE : QR scan 코드 삭제

--- app/src/main/java/com/kusitms/assignmentandroid/MainActivity.java
+++ app/src/main/java/com/kusitms/assignmentandroid/MainActivity.java
@@ -9,7 +9,6 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnScanner;
     Button btnCreateQr;
 
     @Override
@@ -17,24 +16,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnScanner = findViewById(R.id.btn_scanner);
         btnCreateQr = findViewById(R.id.btnCreateQR);
 
-        btnScanner.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, ScannerActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        btnCreateQr.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, CreateQR.class);
-                startActivity(intent);
-            }
+        btnCreateQr.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, CreateQR.class);
+            startActivity(intent);
         });
     }
 }
